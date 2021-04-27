7bffcbcd1d3f8353884f57f7fff1efdb01c27b34
Balint <bogrblint@gmail.com>
2021-04-27 21:11:54 +0200

    Made the requested changes

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,4 +1,5 @@
 package hu.bme.mit.spaceship;
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -13,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private Random generator = new SecureRandom();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
